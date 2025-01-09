61235774d339a813815e480c5b8fb30bc1e254a1
gbhadage4 <164529051+gbhadage4@users.noreply.github.com>
2025-01-09 20:38:28 +0530

    Add files via upload

---
+++ Abstactclass.java
@@ -0,0 +1,19 @@
+package JavaOpps;
+
+public abstract class Abstactclass {
+
+	public int i =10;
+	
+	Abstactclass(){
+		
+		
+	}
+	
+
+	  abstract  void dispaly ();
+	
+	public static void  demo() {
+		
+		System.out.println("static abstarct");
+	}
+}
